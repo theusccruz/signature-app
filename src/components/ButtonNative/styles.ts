e4873f5f19bb76ab72774a7a0759035cb4006ec8
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-06-28 16:26:03 -0300

    Feat:Adicionado componente Botao para native -Refactor: refeito tela de assinatura no native

---
+++ src/components/ButtonNative/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const ButtonContainer = styled.TouchableOpacity`
+  background-color: #020887;
+  height: 100px;
+  width: 100px;
+`;
+
+export const ButtonText = styled.TextInput`
+  color: #fff;
+  font-size: 25px;
+`;
--- src/pages/Signature/index.tsx
+++ src/pages/Signature/index.tsx
@@ -1,16 +1,41 @@
-import React, { useRef, forwardRef } from 'react';
-import SignatureScreen, { SignatureViewRef } from 'react-native-signature-canvas';
-import { View } from 'react-native';
+import React, { useRef } from 'react';
+import SignatureScreen, {
+  SignatureViewRef,
+} from 'react-native-signature-canvas';
+import { View, ViewRow } from './styles';
+import ButtonNative from '../../components/ButtonNative';
 
+interface Props {
+  text: string;
+  onOK: (signature: any) => void;
+}
 
+const Signature: React.FC<Props> = ({ onOK, text }) => {
+  const ref = useRef<SignatureViewRef>(null);
 
-const Signature: React.FC = () => {
+  const handleSignature = (signature: any) => {
+    console.log(signature);
+    onOK(signature);
+  };
+
+  const handleClear = () => {
+    ref.current?.clearSignature();
+  };
+
+  const handleConfirm = () => {
+    console.log('end');
+    ref.current?.readSignature();
+  };
 
   return (
     <View>
-
+      <SignatureScreen ref={ref} onOK={handleSignature} />
+      <ViewRow>
+        <ButtonNative onPress={() => handleClear()}>Limpar</ButtonNative>
+        <ButtonNative onPress={() => handleConfirm()}>Confirmar</ButtonNative>
+      </ViewRow>
     </View>
   );
-}
+};
 
 export default Signature;
